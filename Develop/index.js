a196be517620bf551a736c3faa6aa5855064b89f
amtay <tayloramydesign@gmail.com>
2023-06-27 21:27:36 -0500

    adds questions prompts to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,66 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a short project description?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project?',
+    choices: [
+      'MIT',
+      'APACHE 2.0',
+      'GPL 3.0',
+      'BSD 3',
+      'NONE OF THESE',
+    ],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message:
+      'What does the reader need to know about using this repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Who were the projects contributers?',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
